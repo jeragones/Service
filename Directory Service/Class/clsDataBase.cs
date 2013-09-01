1ab640b005eb388eefd096af53cbf3a6ca989ac5
Jorge Rojas Aragonés <jeragones@gmail.com>
2013-09-01 04:32:06 -0600

    Commit #6

---
+++ Directory Service/Class/clsDataBase.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Directory_Service.Class
+{
+    
+    public class clsDataBase
+    {
+        SqlConnection connection = new SqlConnection("Data Source=.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\DataBase.mdf;Integrated Security=True;User Instance=True");
+
+        /*
+         * 
+         */
+        public void insertar(List<string> columnas, List<string> datos, string tabla)
+        {   
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO " + tabla + " (" + cadena(columnas, false) + ") VALUES (" + cadena(datos, true) + ")";
+            cmd.Connection = connection;
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        /*
+         * 
+         */
+        public void modificar(List<string> columnas, List<string> datos, string[] condicion, string tabla)
+        {
+            string dato;
+            if (numero(condicion[1]))
+                dato = condicion[1];
+            else
+                dato = "'" + condicion[1] + "'";
+            char[] separador = { ',' };
+            string update = cadena(Fragmentar(cadena(columnas, false), separador), Fragmentar(cadena(datos, true), separador));
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "UPDATE " + tabla + " SET " + update + " WHERE " + condicion[0] + " = " + dato;
+            cmd.Connection = connection;
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        /*
+         * 
+         */
+        public void eliminar(string column, string data, string tabla)
+        {
+            string dato;
+            if(numero(data))
+                dato = data;
+            else
+                dato = "'"+data+"'";
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "DELETE FROM " + tabla + " WHERE "+column+" = "+dato;
+            cmd.Connection = connection;
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        /*
+         * 
+         */
+        public ArrayList consultar(List<string> columnas, string[] condicion, string tabla)
+        {
+            ArrayList lista = new ArrayList();
+            string query, dato;
+
+            if (condicion.Length == 0)
+            {
+                query = "SELECT " + cadena(columnas, false) + " FROM " + tabla;
+            }
+            else
+            {
+                if (numero(condicion[1]))
+                    dato = condicion[1];
+                else
+                    dato = "'"+condicion[1]+"'";
+                query = "SELECT " + cadena(columnas, false) + " FROM " + tabla +" WHERE " + condicion[0] + " = " + dato;
+            }
+            
+            connection.Open();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            DataTable result = new DataTable();
+            result.Load(cmd.ExecuteReader());
+            connection.Close();
+
+            foreach (DataRow row in result.Rows)
+            {
+                List<string> list = new List<string>();
+                for (int i = 0; i < columnas.Count; i++)
+                {
+                    list.Add(row[columnas[i]].ToString());
+                }
+                list.Reverse();
+                lista.Add(list);
+            }
+            return lista;
+        }
+
+        /*
+         * 
+         */
+        protected string cadena(List<string> lista, bool tmp) 
+        {
+            string list="";
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (i == 0)
+                    list += tipo(lista[i], tmp);
+                else
+                    list += "," + tipo(lista[i], tmp);
+            }
+            return list;
+        }
+
+        /*
+         * 
+         */
+        protected string cadena(string[] columnas, string[] datos)
+        {
+            string cadena = "";
+            for (int i = 0; i < columnas.Length; i++)
+            {
+                if(i==0)
+                    cadena = columnas[i] + "=" + datos[i];
+                else
+                    cadena += ", " + columnas[i] + "=" + datos[i];
+            }
+            return cadena;
+        }
+
+        /*
+         * 
+         */
+        protected string tipo(string dato, bool tmp)
+        {
+            if (numero(dato))
+                return dato;
+            else
+            {
+                if(tmp)
+                    return "'" + dato + "'";
+                else
+                    return dato; 
+            }
+        }
+
+        /*
+         * 
+         */
+        protected Boolean numero(string num)
+        {
+            int numero = 0;
+            return (int.TryParse(num, out numero));
+        }
+
+        /*
+         * 
+         */
+        protected String[] Fragmentar(String cadena, char[] separador)
+        {
+            String[] vector = cadena.Split(separador);
+            return vector;
+        }
+    }
+}
--- Directory Service/Default.aspx.cs
+++ Directory Service/Default.aspx.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using Directory_Service.Class;
+using System.DirectoryServices;
 
 namespace Directory_Service
 {
     public partial class _Default : System.Web.UI.Page
     {
+
+        clsDataBase insBD = new clsDataBase();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -18,8 +25,75 @@ namespace Directory_Service
         {
             if (!txtUsuario.Text.Equals("") & !txtPass.Text.Equals(""))
             {
+                
+
                 Response.Write("<script type='text/javascript'>window.open('GUI/frmInicio.aspx','_parent');</script>");
             }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            List<string> c = new List<string> {"usuario","nombre","contraseña","grupo"};
+            List<string> d = new List<string> { "jdaniel", "Daniel Berrocal", "Jda.123", "5" };
+            insBD.insertar(c, d, "Usuario");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            List<string> c = new List<string> { "nombre", "contraseña" };
+            List<string> d = new List<string> { "Daniel Berrocal Ramirez", "Jda.123999" };
+            string[] s = {"usuario","jdaniel"};
+            insBD.modificar(c, d, s,"Usuario");
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            insBD.eliminar("usuario","jdaniel","Usuario");
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            List<string> c = new List<string> { "nombre", "contraseña" };
+            string[] s = { "usuario", "jdaniel" };
+            insBD.consultar(c,s,"Usuario");
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            List<string> c = new List<string> { "usuario","nombre", "contraseña", "grupo" };
+            string[] s = { };
+            insBD.consultar(c, s, "Usuario");
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            CreateUserAccount("LDAP:/" + "/host:172.16.0.1", "mijo", "Menoriti.5698");
+        }
+
+        public string CreateUserAccount(string ldapPath, string userName, string userPassword)
+        {
+            try
+            {
+                string oGUID = string.Empty;
+                string connectionPrefix = ldapPath;
+                DirectoryEntry dirEntry = new DirectoryEntry(connectionPrefix);
+                DirectoryEntry newUser = dirEntry.Children.Add
+                    ("CN=" + userName, "user");
+                newUser.Properties["samAccountName"].Value = userName;
+                newUser.CommitChanges();
+                oGUID = newUser.Guid.ToString();
+
+                newUser.Invoke("SetPassword", new object[] { userPassword });
+                newUser.CommitChanges();
+                dirEntry.Close();
+                newUser.Close();
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException E)
+            {
+                //DoSomethingwith --> E.Message.ToString();
+
+            }
+            return "";
         }
     }
 }
