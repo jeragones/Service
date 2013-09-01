5bfc107d295a14e92ec4edc2564ab4fa6c976d31
daniel berrocal <jdanielb1992@gmail.com>
2013-09-01 03:35:41 -0600

    service

---
+++ Jump/WS/WS/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace WS
+{
+    public partial class SW : ServiceBase
+    {
+        public SW()
+        {
+            InitializeComponent();
+            if(!System.Diagnostics.EventLog.SourceExists("MySourse")){
+                System.Diagnostics.EventLog.CreateEventSource("MySourse","MyNewLog");
+            }
+            eventLog1.Source = "MySourse";
+            eventLog1.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Incie!!");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Termine!!");
+        }
+    }
+}
